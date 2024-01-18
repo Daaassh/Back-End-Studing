0d1b84611a24539ba2f0f664262497655232bc4e
Daaassh <dr.polar099@gmail.com>
2024-01-18 14:00:04 -0300

    Aula 01. Finalizada

---
+++ Aula01/src/js/script.js
@@ -0,0 +1,10 @@
+var name = "Hello, World!";
+function print(text) {
+  console.log(text);
+}
+function som(number1,number2){
+  var result = number1 + number2
+  print(result)
+}
+som(2,3)
+print(name);
